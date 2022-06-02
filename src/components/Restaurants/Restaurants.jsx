72c99b8dd1ce8f34936a6a4fc1462638610cbeaf
Anisimov Artem <a.anisimov@runetsoft.ru>
2022-06-02 19:59:04 +0300

    Hometask №3 | add active tabs display

--- src/components/Restaurants/Restaurants.jsx
+++ src/components/Restaurants/Restaurants.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Restaurant } from "../Restaurant/Restaurant";
+import {Restaurant} from "../Restaurant/Restaurant";
 
 import styles from './styles.module.css';
 
@@ -8,16 +8,14 @@ export const Restaurants = ({restaurants}) => {
 
     return (<div className={styles.root}>
         <div>
-            <div className={styles.tab_wrapper}>
-                {restaurants.map(({name}, index) => (
-                    <button
-                        key={index}
-                        onClick={()=>setActiveRestaurant(index)}
-                        className={styles.tab_button}
-                    >{name}</button>
-                ))}
-            </div>
-            <Restaurant restaurant={restaurants[activeRestaurant]} />
+            {restaurants.map(({name}, index) => (
+                <button
+                    key={index}
+                    onClick={() => setActiveRestaurant(index)}
+                    className={activeRestaurant === index ? styles.active : null}
+                >{name}</button>
+            ))}
+            <Restaurant restaurant={restaurants[activeRestaurant]}/>
         </div>
     </div>)
 }
