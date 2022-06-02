3fc8c5145a5064c51446977f57c69d7940fca367
Stepan <egovip@mail.ru>
2022-06-02 21:55:18 +0700

    home task 3

--- src/components/Restaurants/Restaurants.jsx
+++ src/components/Restaurants/Restaurants.jsx
@@ -1,14 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Restaurant } from "../Restaurant/Restaurant";
 
 import styles from './styles.module.css';
 
-export const Restaurants = ({restaurants}) => {
+export const Restaurants = ({ restaurants }) => {
     const [activeRestaurant, setActiveRestaurant] = useState(0);
 
     return (<div className={styles.root}>
         <div>
-            <Restaurant restaurant={restaurants[activeRestaurant]}/>
+            <div className={styles.btn_wrapper}>
+                <button
+                    onClick={()=>setActiveRestaurant(0)}
+                    className={styles.btn}
+                >Dishoom
+                </button>
+                <button
+                    onClick={()=>setActiveRestaurant(1)}
+                    className={styles.btn}
+                >Homeslice
+                </button>
+                <button
+                    onClick={()=>setActiveRestaurant(2)}
+                    className={styles.btn}
+                >Fabrique
+                </button>
+                <button
+                    onClick={()=>setActiveRestaurant(3)}
+                    className={styles.btn}
+                >Flat Iron
+                </button>
+            </div>
+            <Restaurant restaurant={restaurants[activeRestaurant]} />
         </div>
     </div>)
 }
