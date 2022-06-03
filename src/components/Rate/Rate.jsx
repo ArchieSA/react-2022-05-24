be5f6cee79090717997dbaacbed0287a2e3c8a35
Anisimov Artem <a.anisimov@runetsoft.ru>
2022-06-03 11:27:43 +0300

    Hometask №2 | fixes after review

--- src/components/Rate/Rate.jsx
+++ src/components/Rate/Rate.jsx
@@ -6,11 +6,8 @@ import starGold from './img/star-gold.svg';
 export const Rate = ({rate}) => {
     return (
         <div className={styles.root}>
-            {[...Array(5).keys()].map(
-                (_, i) => i < rate
-                    ? <img key={i} className={styles.icon} src={starGold} />
-                    : <img key={i} className={styles.icon} src={starEmpty} />
-            )}
+            {[...Array(5).keys()].map((_, i) => (
+                <img key={i} className={styles.icon} src={i < rate ? starGold : starEmpty}/>))}
         </div>
     );
 }
