5a6189749c173666f884c25f3c91583b4a2e4310
Anisimov Artem <a.anisimov@runetsoft.ru>
2022-06-13 14:57:51 +0300

    hometask 4 | fixes after code review

--- src/store/basket/selectors.js
+++ src/store/basket/selectors.js
@@ -2,3 +2,9 @@ export const selectBasket = (state) => state.basket;
 
 export const selectProductCountFromBasket = (state, productId) =>
   selectBasket(state)[productId] || 0;
+
+export const selectProductIdsFromBasket = (state) => {
+  const basket = selectBasket(state);
+
+  return Object.keys(basket).filter((productId) => !!basket[productId]);
+};
