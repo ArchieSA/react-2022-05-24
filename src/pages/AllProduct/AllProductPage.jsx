98e43b895a9347779995c626a5b3b678a483ea80
Anton Timofeev <maestroat@yandex.ru>
2022-06-22 18:22:25 +0300

    lesson_8_1

--- src/pages/AllProduct/AllProductPage.jsx
+++ src/pages/AllProduct/AllProductPage.jsx
@@ -6,16 +6,18 @@ import styles from "../../components/Menu/styles.module.css";
 import ProductContainer from "../../containers/Product/Product";
 import {useSelector} from "react-redux";
 import {selectProductIds} from "../../store/product/selectors";
+import {Products} from "../../components/Products/Products";
 
 export const AllProductPage = () => {
     const productIds = useSelector((state) =>
         selectProductIds(state)
     );
+
   return (
       <Layout>
           <ErrorBoundary>
               {productIds?.map((productId) => (
-          <ProductContainer
+          <Products
               key={productId}
               productId={productId}
               className={styles.product}
