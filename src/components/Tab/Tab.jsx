5afbe55031f67fb860f313be83fff83a648c04a9
Anton Timofeev <maestroat@yandex.ru>
2022-07-12 19:09:11 +0300

    lesson_8_12

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -5,10 +5,11 @@ import PropTypes from "prop-types";
 import styles from "./styles.module.css";
 import { NavLink } from "react-router-dom";
 
-export const Tab = ({ label, id, className }) => {
+export const Tab = ({ label, id, className, date, dateLater }) => {
+    // console.log(date(id));
   return (
     <NavLink
-      to={`/restaurants/${id}`}
+      to={`/restaurants/${date(id)}/${dateLater(label)}`}
       className={({ isActive }) =>
         classnames(styles.root, className, {
           [styles.selected]: isActive,
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import { Tab } from "../Tab/Tab";
 
 import styles from "./styles.module.css";
-
+const date = (id) => id;
+const dateLater = (label) => label;
 export const Tabs = ({ tabs }) => (
   <div className={styles.root}>
     {tabs.map(({ label, id }) => (
-      <Tab key={id} label={label} id={id} className={styles.tab} />
+      <Tab key={id} label={label} id={id} className={styles.tab} date={date} dateLater={dateLater} />
     ))}
   </div>
 );
